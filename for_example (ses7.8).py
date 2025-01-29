21c23db481745e68c6ac572e20c9d7ec364e0e8f
JFmg007 <jf.manidoni@gmail.com>
2025-01-29 16:46:02 +0100

    added for example

---
+++ for_example (ses7.8).py
@@ -0,0 +1,23 @@
+# sum the first 10 numbers
+sum = 0
+for num in range(1,101): # last one is excluded, so it goes from 1 to 100
+    sum = sum + num
+print(sum)
+
+# rewrite it using while loop ^
+sum = 0
+num = -101
+while num < 100:
+    num = num + 1
+    sum += num# same thing as sum = sum + 1
+    print(sum)
+
+# print multiplication table
+for i in range(1, 11):
+    for j in range(1, 11):
+        print(f"{i} * {j} = {i * j}")
+    print() # to separate  with a new line between blocks
+
+i=20
+print("i i i ")
+print(f"{i} {21+1} i i ")
--- readme.md
+++ readme.md
@@ -3,3 +3,4 @@
 ## Exception
 ## If
 ## While
+## For
